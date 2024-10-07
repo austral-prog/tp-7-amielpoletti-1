ddacfa3242e43d420ca44df220581731cb441aef
amielpoletti <162723122+amielpoletti@users.noreply.github.com>
2024-10-07 20:44:14 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,19 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(lista):
+    enumerated_list = []
+    
+    for i, value in enumerate(lista):
+        if value != "":
+            enumerated_list.append(f"{i}. {value}")
+    return enumerated_list
 
 
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(lista):
+    enumerated_backwards_list = []
+    
+    for i, value in enumerate(lst):
+        if value != "":
+            enumerated_backwards_list.append(f"{i}. {value[::-1]}")
+    
+    return enumerated_backwards_list
+
+
