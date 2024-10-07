8b56acbb138caa304e548241206937a93571949f
amielpoletti <162723122+amielpoletti@users.noreply.github.com>
2024-10-07 20:54:40 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,33 @@
-def index_of_by_index(word, list, index):
+def index_of_by_index(word, lista, index):
+    for i, value in enumerate(lista[index:], start=index):
+        if value == word:
+            return i
+    return -1
+
+def index_of_empty(lista):
+    for i, value in enumerate(lista):
+        if value == "":
+            return i
     return -1
 
 
-def index_of_empty(list):
+def index_of(word, lista):
+    for i, value in enumerate(lista):
+        if value == word:
+            return i
     return -1
 
-
-def index_of(word, list):
+def put(word, lista):
+    for i, value in enumerate(lista):
+        if value == "":
+            lista[i] = word
+            return i
     return -1
 
-
-def put(word, list):
-    return -1
-
-
-def remove(word, list):
-    return -1
+def remove(word, lista):
+    count = 0
+    for i, value in enumerate(lista):
+        if value == word:
+            lista[i] = ""
+            count += 1
+    return count
